5c76fb9358725bc01a7e72dd6d6a19343298bd90
marclou <marc.louvion@gmail.com>
2018-03-28 15:56:54 +0900

    es7 support for all queries

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-//http://api.fixer.io/latest?base=USD
-//https://restcountries.eu/rest/v2/currency/currencyCode
-
 const axios = require('axios');
 
+// ES6 PROMISES
 const getExchangeRate = (from, to) => {
     return axios
         .get(`http://api.fixer.io/latest?base=${from}`)
@@ -23,31 +21,50 @@ const getCountries = (currency) => {
         .catch((e) => console.log(e));
 };
 
-// ES6 PROMISES
-// const convertAndAnalyseCurrency = (from, to, amount) => {
-//     return getCountries(to)
-//         .then((countries) => {
-//
-//             return getExchangeRate(from, to)
-//                 .then((exchangeCurrency) => {
-//                     const exchangeAmount = exchangeCurrency * amount;
-//
-//                     return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} is available in ${countries.join(', ')}`;
-//                 });
-//         });
-// };
+const convertAndAnalyseCurrencyAsync = (from, to, amount) => {
+    return getCountries(to)
+        .then((countries) => {
+
+            return getExchangeRate(from, to)
+                .then((exchangeCurrency) => {
+                    const exchangeAmount = exchangeCurrency * amount;
+
+                    return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} is available in ${countries.join(', ')}`;
+                });
+        });
+};
 
 //ES7 ASYNC AWAIT
+const getExchangeRateAsync = async (from, to) => {
+	try {
+		const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+
+		return response.data.rates[to];
+
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const getCountriesAsync = async (currency) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`);
+
+        return response.data.map(country => country.name);
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
 const convertAndAnalyseCurrency = async (from, to, amount) => {
-    const countries = await getCountries(to);
-    const exchangeCurrency = await getExchangeRate(from, to);
+    const countries = await getCountriesAsync(to);
+    const exchangeCurrency = await getExchangeRateAsync(from, to);
     const exchangeAmount = exchangeCurrency * amount;
 
     return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} is available in ${countries.join(', ')}`;
 };
 
 
-
-
-
 convertAndAnalyseCurrency('EUR', 'CAD', 100).then((res) => console.log(res));
+convertAndAnalyseCurrencyAsync('EUR', 'CAD', 100).then((res) => console.log(res));
